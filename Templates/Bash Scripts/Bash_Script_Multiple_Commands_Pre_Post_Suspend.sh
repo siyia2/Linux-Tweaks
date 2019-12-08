8d239089a1addbabb33a412b8c6aa59f768680f4
siyia2 <46220960+siyia2@users.noreply.github.com>
2019-12-08 14:17:40 +0200

    Create Bash_Script_Multiple_Commands_Pre_Post_Suspend.sh

---
+++ Templates/Bash Scripts/Bash_Script_Multiple_Commands_Pre_Post_Suspend.sh
@@ -0,0 +1,12 @@
+case $1 in
+    pre)
+        #Enter Command to execute before suspend
+         wait
+        done
+    ;;
+    post)
+        #Enter Command to execute after suspend
+        wait
+        done
+    ;;
+esac
