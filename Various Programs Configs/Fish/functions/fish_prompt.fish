0b22788e501aed5cbf69c9905b70b3d7cb28fd88
siyia2 <46220960+siyia2@users.noreply.github.com>
2020-05-08 20:15:21 +0300

    Add files via upload

---
+++ Various Programs Configs/Fish/functions/fish_prompt.fish
@@ -0,0 +1,35 @@
+function fish_prompt --description 'Write out the prompt'
+	set -l last_status $status
+
+    # User
+    set_color $fish_color_command
+    echo -n (whoami)
+    set_color purple
+
+    echo -n '@'
+
+    # Host
+    set_color $fish_color_command
+    echo -n (prompt_hostname)
+    set_color $fish_color_command
+
+     echo -n ':'
+
+    # PWD
+    set_color $fish_color_comment
+    echo -n (prompt_pwd)
+    set_color normal
+
+    __terlar_git_prompt
+    __fish_hg_prompt
+    echo
+
+    if not test $last_status -eq 0
+        set_color $fish_color_error
+	else 
+		set_color $fish_color_command
+    end
+
+    echo -n '><> '
+
+end
