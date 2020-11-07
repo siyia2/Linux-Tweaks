789d3b1881819ac2ae087bb0803f2522ff59a11c
siyia2 <46220960+siyia2@users.noreply.github.com>
2020-11-08 01:09:01 +0200

    Update gpu_temp.sh

--- Wayland Setup/waybar/modules/gpu_temp.sh
+++ Wayland Setup/waybar/modules/gpu_temp.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-gpu_temp=$(less /sys/class/hwmon/hwmon3/temp1_input | grep -Po '.*(?=...$)')
+gpu_temp=$(cat /sys/class/hwmon/hwmon3/temp1_input | grep -Po '.*(?=...$)')
 echo "$gpu_temp"
