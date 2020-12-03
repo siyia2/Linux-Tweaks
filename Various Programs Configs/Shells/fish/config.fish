91c6251299e9d293aa441c11db27e789ce9559b6
siyia2 <46220960+siyia2@users.noreply.github.com>
2020-12-03 23:31:55 +0200

    Update config.fish

--- Various Programs Configs/Shells/fish/config.fish
+++ Various Programs Configs/Shells/fish/config.fish
@@ -1,1 +1,8 @@
 #screenfetch -w
+
+# Changing "ls" to "exa"
+alias ls='exa -al --color=always --group-directories-first' # my preferred listing
+alias la='exa -a --color=always --group-directories-first'  # all files and dirs
+alias ll='exa -l --color=always --group-directories-first'  # long format
+alias lt='exa -aT --color=always --group-directories-first' # tree listing
+alias l.='exa -a | egrep "^\."'
