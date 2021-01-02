394a8249d1426a0fd66a8104541a8c498f862e95
siyia2 <46220960+siyia2@users.noreply.github.com>
2021-01-02 17:22:07 +0200

    Delete vram.sh

--- Wayland Setup/waybar/modules/vram.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-gpu_vram_mb=$(cat /sys/class/drm/card0/device/mem_info_vram_used | grep -Po '.*(?=......$)')
-vram_total=4096
-vram_percent=$(awk "BEGIN { pc=100*${gpu_vram_mb}/${vram_total}; i=int(pc); print (pc-i<0.5)?i:i+1 }")
-echo $vram_percent
-exit
