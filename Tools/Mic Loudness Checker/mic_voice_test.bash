e59be05cb54a53c48607f849f30e87b268a977cc
siyia2 <46220960+siyia2@users.noreply.github.com>
2022-06-28 18:44:27 +0300

    Update mic_voice_test.bash

--- Tools/Mic Loudness Checker/mic_voice_test.bash
+++ Tools/Mic Loudness Checker/mic_voice_test.bash
@@ -1,8 +1,8 @@
 #!/bin/bash
 RP=/tmp/volume
 RPR=/tmp/volume/recordings
-DAY_VOLUME_THRESHOLD='0.1'
-NIGHT_VOLUME_THRESHOLD='0.05'
+DAY_VOLUME_THRESHOLD='0.2'
+NIGHT_VOLUME_THRESHOLD='0.1'
 H=$(date +%H)
 
 if [ ! -d $RP ]
