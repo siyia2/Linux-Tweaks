087de23c916409d77dda3768988c0df237315a86
siyia2 <46220960+siyia2@users.noreply.github.com>
2022-06-29 12:09:25 +0300

    Update mic_voice_test.bash

--- Tools/Mic Loudness Checker/mic_voice_test.bash
+++ Tools/Mic Loudness Checker/mic_voice_test.bash
@@ -1,7 +1,7 @@
 #!/bin/bash
 RP=/tmp/volume
 RPR=/tmp/volume/recordings
-DAY_VOLUME_THRESHOLD='0.3'
+DAY_VOLUME_THRESHOLD='0.25'
 NIGHT_VOLUME_THRESHOLD='0.1'
 H=$(date +%H)
 
@@ -24,7 +24,7 @@ do
 if [ "22" == "$H" ] || [ "21" == "$H" ] || [ "20" == "$H" ] || [ "19" == "$H" ] || [ "18" == "$H" ] || [ "17" == "$H" ] || [ "16" == "$H" ] || [ "15" == "$H" ] || [ "14" == "$H" ] || [ "13" == "$H" ] || [ "12" == "$H" ] || [ "11" == "$H" ] || [ "10" == "$H" ] || [ "09" == "$H" ] || [ "08" == "$H" ] || [ "07" == "$H" ] 
 then
 TIME=`date +%s`
-VOLUME=`arecord -qd 5 volt && sox volt -n stat &> volt.d && sed '4q;d' volt.d | awk '{print $3}'`
+VOLUME=`arecord -f S16_LE -qd 5 volt && sox volt -n stat &> volt.d && sed '4q;d' volt.d | awk '{print $3}'`
 echo "$VOLUME"
 
 if [ 1 -eq "$(echo "${VOLUME} > ${DAY_VOLUME_THRESHOLD}" | bc)" ]
@@ -38,7 +38,7 @@ fi
 if [ "23" == "$H" ] || [ "00" == "$H" ] || [ "01" == "$H" ] || [ "02" == "$H" ] || [ "03" == "$H" ] || [ "04" == "$H" ] || [ "05" == "$H" ] || [ "06" == "$H" ]
 then
 TIME=`date +%s`
-VOLUME=`arecord -qd 5 volt && sox volt -n stat &> volt.d && sed '4q;d' volt.d | awk '{print $3}'`
+VOLUME=`arecord -f S16_LE -qd 5 volt && sox volt -n stat &> volt.d && sed '4q;d' volt.d | awk '{print $3}'`
 echo "$VOLUME"
 
 if [ 1 -eq "$(echo "${VOLUME} > ${NIGHT_VOLUME_THRESHOLD}" | bc)" ]
