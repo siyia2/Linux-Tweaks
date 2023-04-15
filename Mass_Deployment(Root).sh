96a0f6a37c082a12de696c147726ab22a0243279
siyia2 <46220960+siyia2@users.noreply.github.com>
2023-04-15 18:25:38 +0300

    Update Mass_Deployment(Root).sh

--- Mass_Deployment(Root).sh
+++ Mass_Deployment(Root).sh
@@ -40,7 +40,7 @@ if [ $? -eq 0 ]; then
               # Ask the user for regular or low RAM scenario
               echo " "
               echo "1. Regular scenario"
-              echo "2. Low RAM scenario (Combine with Zram)"
+              echo "2. Low RAM scenario (Only for <4GB Ram & combine with Zram)"
               read -p "Enter your choice: " iocache_choice
 
               case $iocache_choice in
