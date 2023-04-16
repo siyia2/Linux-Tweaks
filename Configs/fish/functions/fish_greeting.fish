6d91104ba373351244080eefbfbcc4456776b2a8
siyia2 <46220960+siyia2@users.noreply.github.com>
2023-04-16 14:22:39 +0300

    Add files via upload

--- Configs/fish/functions/fish_greeting.fish
+++ Configs/fish/functions/fish_greeting.fish
@@ -1,6 +1,7 @@
 function fish_greeting                                            
-echo 'Welcome To The Terminal!'
-echo 'Package Managers: "pacman", "yay" (aur)'
-echo 'Basic Functions: "-Ss" (search), "-S" (install), "-R" (remove)
-'
+echo 'Package Managers:'(set_color cyan --bold)'"pacman", "yay" (aur)'(set_color normal) 
+echo "Functions:"(set_color green --bold)'"-Ss" (search), "-S" (install), "-R" (remove),'(set_color normal) (set_color red --bold)'"-Rdd" (remove w/o dependencies), "-Rns (pacman -Qtdq)" (remove orphans)'(set_color normal)
+
+echo Recursively Chown: (set_color green --bold)'"chown -R USER:GROUP FILE"'(set_color normal) Recursively RW: (set_color green --bold)'"chmod -R 775 FILE"'(set_color normal) Make Executable: (set_color green --bold)'"chmod +x FILE"'(set_color normal)
+echo " "
 end 
