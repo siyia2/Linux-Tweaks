75d0ef0156c894694a552719176e50d2b644a2c3
siyia2 <46220960+siyia2@users.noreply.github.com>
2023-04-16 14:25:07 +0300

    Add files via upload

--- Configs/fish/functions/fish_greeting.fish
+++ Configs/fish/functions/fish_greeting.fish
@@ -2,6 +2,6 @@ function fish_greeting
 echo 'Package Managers:'(set_color cyan --bold)'"pacman", "yay" (aur)'(set_color normal) 
 echo "Functions:"(set_color green --bold)'"-Ss" (search), "-S" (install), "-R" (remove),'(set_color normal) (set_color red --bold)'"-Rdd" (remove w/o dependencies), "-Rns (pacman -Qtdq)" (remove orphans)'(set_color normal)
 
-echo Recursively Chown: (set_color green --bold)'"chown -R USER:GROUP FILE"'(set_color normal) Recursively RW: (set_color green --bold)'"chmod -R 775 FILE"'(set_color normal) Make Executable: (set_color green --bold)'"chmod +x FILE"'(set_color normal)
+echo Recursively Chown: (set_color green --bold)'"chown -R USER:GROUP FILE"'(set_color normal) Recursively RW: (set_color green --bold)'"chmod -R 700 FILE"'(set_color normal) Make Executable: (set_color green --bold)'"chmod +x FILE"'(set_color normal)
 echo " "
 end 
