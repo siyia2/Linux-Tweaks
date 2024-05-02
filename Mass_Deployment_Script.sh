a39b45f2c8edf4e0ad42e103290adf962070f5b9
siyia2 <46220960+siyia2@users.noreply.github.com>
2024-05-02 22:23:01 +0300

    Add files via upload

--- Mass_Deployment_Script.sh
+++ Mass_Deployment_Script.sh
@@ -1,228 +1,245 @@
 #!/bin/bash
-var=0
-while [ $var -ne 3 ]; do
-  echo "Press 1 for Non-Root Options"
-  echo "Press 2 for Root Options"
-  echo "Press 3 to exit"
-  read -p "Enter your choice: " choice
-  
-  case $choice in
-    1)
-SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
-CONFIG_DIR="$SCRIPT_DIR/Configs"
-WAYLAND_SETUP_DIR="$SCRIPT_DIR/Wayland Setup"
-wayland_choice=0
-prev_choice=0
-while true; do
-  echo ""
 
-  case $choice in
-    1)
-      echo "Press 1 to Deploy Config Files to ~/"
-      echo "Press 2 to Deploy a Wayland Desktop"
-      echo "Press 3 to exit"
+# Colors
+RED='\033[0;31m'
+GREEN='\033[0;32m'
+YELLOW='\033[0;33m'
+BLUE='\033[0;34m'
+PURPLE='\033[0;35m'
+CYAN='\033[0;36m'
+NC='\033[0m' # No Color
 
-      read -p "Enter your choice: " subchoice
-      echo ""
-prev_choice=$subchoice
-      case $subchoice in
-        1)
-          cd "$CONFIG_DIR" || exit
-          ls
+clear
+# Function to print the menu
+print_menu() {
+    echo -e "${CYAN}╔════════════════════════════╗${NC}"
+    echo -e "${CYAN}║${PURPLE}        Main Menu           ${CYAN}║${NC}"
+    echo -e "${CYAN}╠════════════════════════════╣${NC}"
+    echo -e "${CYAN}║${GREEN} 1 Non-Root Options         ${CYAN}║${NC}"
+    echo -e "${CYAN}║${GREEN} 2 Root Options            ${CYAN} ║${NC}"
+    echo -e "${CYAN}║${RED} 3 exit                    ${CYAN} ║${NC}"
+    echo -e "${CYAN}╚════════════════════════════╝${NC}"
+}
 
-          echo "Enter 'A' to deploy all config files, or enter specific filename(s) separated by space, press 'q' to quit at any time:"
-while true; do    
-  read files
-  for files in $files; do
-    if [ "$files" == "A" ]; then
-      cp -r * ~/.config/
-      cp rtorrent.rc ~/.rtorrent.rc 
-      cp zshrc ~/.zshrc
-      rm ~/.config/rtorrent.rc
-      rm ~/.config/zshrc
-      echo "Config files deployed successfully!"
-    elif [ "$files" == "zshrc" ]; then 
-      cp zshrc ~/.zshrc
-      echo "Config files deployed successfully!"
-    elif [ "$files" == "rtorrent.rc" ]; then 
-      cp rtorrent.rc ~/.rtorrent.rc
-      echo "Config files deployed successfully!"
-    elif [ "$files" == "q" ]; then
-      echo "exiting..."
-      break 2
-    else
-      cp -r "$files" ~/.config
-      echo "Config files deployed successfully!"
+# Function to print non-root options menu
+print_non_root_options() {
+    echo -e "${CYAN}╔═══════════════════════════════════╗${NC}"
+    echo -e "${CYAN}║${PURPLE}      Non-Root Options             ${CYAN}║${NC}"
+    echo -e "${CYAN}╠═══════════════════════════════════╣${NC}"
+    echo -e "${CYAN}║${GREEN} 1 Deploy Config Files into ~/     ${CYAN}║${NC}"
+    echo -e "${CYAN}║${GREEN} 2 Deploy a Wayland Desktop        ${CYAN}║${NC}"
+    echo -e "${CYAN}║${RED} 3 go back                         ${CYAN}║${NC}"
+    echo -e "${CYAN}╚═══════════════════════════════════╝${NC}"
+}
+
+# Function to print root options menu
+print_root_options() {
+    echo -e "${CYAN}╔══════════════════════════════╗${NC}"
+    echo -e "${CYAN}║${PURPLE}        Root Options          ${CYAN}║${NC}"
+    echo -e "${CYAN}╠══════════════════════════════╣${NC}"
+    echo -e "${CYAN}║${GREEN} 1. I/O & Cache               ${CYAN}║${NC}"
+    echo -e "${CYAN}║${GREEN} 2. MGLRU                     ${CYAN}║${NC}"
+    echo -e "${CYAN}║${GREEN} 3. ZRAM                      ${CYAN}║${NC}"
+    echo -e "${CYAN}║${RED} 4. Exit                      ${CYAN}║${NC}"
+    echo -e "${CYAN}╚══════════════════════════════╝${NC}"
+}
+
+# Function to handle non-root options
+non_root_options() {
+    SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
+    CONFIG_DIR="$SCRIPT_DIR/Configs"
+    WAYLAND_SETUP_DIR="$SCRIPT_DIR/Wayland Setup"
+
+    while true; do
+    clear
+        print_non_root_options
+        read -rp "Enter your choice: " subchoice
+
+        case "$subchoice" in
+            1)
+                cd "$CONFIG_DIR" || exit
+                ls
+
+                echo "Enter 'A' to deploy all config files, or enter specific filename(s) separated by space, press 'q' to quit:"
+                while true; do
+                    read -rp "Files: " files
+                   if [ "$files" = "q" ] || [ -z "$files" ]; then
+                        break
+                    elif [ "$files" = "A" ]; then
+                        cp -r * ~/.config/
+                        cp rtorrent.rc ~/.rtorrent.rc
+                        cp zshrc ~/.zshrc
+                        rm ~/.config/rtorrent.rc ~/.config/zshrc
+                        echo "Config files deployed successfully!"
+                    else
+                        for file in $files; do
+                            if [ "$file" = "zshrc" ]; then
+                                cp zshrc ~/.zshrc
+                            elif [ "$file" = "rtorrent.rc" ]; then
+                                cp rtorrent.rc ~/.rtorrent.rc
+                            else
+                                cp -r "$file" ~/.config
+                            fi
+                        done
+                        echo "Config files deployed successfully!"
+                    fi
+                done
+                ;;
+            2)
+                while true; do
+                clear
+                    echo "Press 1 for Hyprland"
+                    echo "Press 2 for Sway"
+                    echo "Press 3 to go back"
+                    read -rp "Enter your choice: " wayland_choice
+
+                    case "$wayland_choice" in
+                        1)
+                            cd "$WAYLAND_SETUP_DIR/Hyprland" || exit
+                            cp -r * ~/.config/
+                            echo "Hyprland Desktop deployed successfully!"
+                            ;;
+                        2)
+                            cd "$WAYLAND_SETUP_DIR/Sway" || exit
+                            cp -r * ~/.config/
+                            echo "Sway Desktop deployed successfully!"
+                            ;;
+                        3)
+                            break
+                            ;;
+                        *)
+                            echo "Invalid choice"
+                            ;;
+                    esac
+                done
+                ;;
+            3)
+                break
+                ;;
+            *)
+                echo "Invalid choice"
+                ;;
+        esac
+    done
+}
+
+# Function to handle root options
+root_options() {
+    SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
+
+    # Prompt the user for the root password
+    echo -n "Enter root password: "
+    read -rs password
+
+    # Check if the user pressed escape
+    if [ -z "$password" ]; then
+        exit
     fi
-  done
-done
-;;
-        2)
-          echo "Press 1 for Hyprland" 
-          echo "Press 2 for Sway"
-          echo "3. ↩"
-          read -p "Enter your choice: " wayland_choice
-          echo ""
 
-          case $wayland_choice in
-            1)
-              cd "$WAYLAND_SETUP_DIR/Hyprland" || exit
-              cp -r * ~/.config/
-              echo "Hyprland Desktop deployed successfully!"
-              ;;
-            2)
-              cd "$WAYLAND_SETUP_DIR/Sway" || exit
-              cp -r * ~/.config/
-              echo "Sway Desktop deployed successfully!"
-              ;;
-            3)
-              wayland_choice=$subchoice
-              ;;
-            *)
-              echo "Invalid choice"
-              ;;
-          esac
-          ;;
-          3)
-          break
-          ;;
-        *)
-          echo "Invalid choice"
-          ;;
-      esac
-      ;;
-    *)
-      echo "Invalid choice"
-      ;;
-  esac
-done
+    # Authenticate the user as root
+    echo "$password" | sudo -S true 2>/dev/null
 
-;;
+    if [ $? -eq 0 ]; then
+        # The user is authenticated as root
 
-2)
-
-# Set the script directory
-SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
-choice=0
-prev_choice=0
-iocache_choice=0
-# Prompt the user for the root password
-echo -n "Enter root password: "
-read -s password
-
-# Check if the user pressed escape
-if [ -z "$password" ]; then
-  exit
-fi
-
-# Authenticate the user as root
-echo "$password" | sudo -S true 2>/dev/null
-
-if [ $? -eq 0 ]; then
-  # The user is authenticated as root
-
-  # Show the root options menu
+        # Show the root options menu
         while true; do
-          echo " "
-          echo "1. I/O&Cache"
-          echo "2. MGLRU"
-          echo "3. ZRAM"
-          echo "4. Exit"
-          read -p "Enter your choice: " subchoice
-prev_choice=$choice
-          case $subchoice in
-            1)
-              # Ask the user for regular or low RAM scenario
-              echo " "
-              echo "1. Regular scenario"
-              echo "2. Low RAM scenario (Only for =<4GB Ram & combine with Zram)"
-              echo "3. ↩"
-              read -p "Enter your choice: " iocache_choice
-              
-              case $iocache_choice in
-                1)
-                  # Copy files for regular scenario and execute sysctl -p
-                  echo "Copying files for regular scenario..."
-                  dir="/etc/udev/rules.d"
-                  dir2="/etc/sysctl.d/"
-                  if [ ! -d "$dir" ]; then
-                   sudo mkdir -p "$dir"
-                  fi
-                  sudo cp "$SCRIPT_DIR/Optimizations/IO-Schedulers-Cache-Configs/60-ioschedulers.rules" /etc/udev/rules.d/60-ioschedulers.rules
-                  if [ ! -d "$dir2" ]; then
-                   sudo mkdir -p "$dir2"
-                  fi
-                  sudo cp "$SCRIPT_DIR/Optimizations/IO-Schedulers-Cache-Configs/sysctl.conf" /etc/sysctl.d/99-sysctl.conf
-                  sudo sysctl --system
-                  ;;
-                2)
-                  # Copy files for low RAM scenario and execute sysctl -p
-                  echo "Copying files for low RAM scenario..."
-                  dir="/etc/udev/rules.d"
-                  dir2="/etc/sysctl.d/"
-                  if [ ! -d "$dir" ]; then
-                   sudo mkdir -p "$dir"
-                  fi
-                  sudo cp "$SCRIPT_DIR/Optimizations/IO-Schedulers-Cache-Configs/60-ioschedulers.rules" /etc/udev/rules.d/60-ioschedulers.rules
-                  if [ ! -d "$dir2" ]; then
-                   sudo mkdir -p "$dir2"
-                  fi
-                  sudo cp "$SCRIPT_DIR/Optimizations/IO-Schedulers-Cache-Configs/sysctl_low_ram.conf" /etc/sysctl.d/99-sysctl.conf
-                  sudo sysctl --system
-                  ;;
-                3)
-                 iocache_choice=$prev_choice # Set choice to previous menu choice
-              ;; 
-               
-                *)
-                  # Invalid iocache_choice
-                  echo "Invalid choice"
-                  ;;
-              esac
-              ;;
-            2)
-              # Code for MGLRU option
-    echo "Installing MGLRU..."
-    sudo cp "$SCRIPT_DIR/Optimizations/MGLRU/mglru" /bin/mglru
-    sudo chmod +x /bin/mglru
-    sudo cp "$SCRIPT_DIR/Optimizations/MGLRU/mglru.service" /lib/systemd/system/mglru.service
-    sudo systemctl enable --now mglru.service
-    echo "MGLRU installed and enabled"
-    ;;
-            3)
-# Code for ZRAM option
-    echo "Installing ZRAM..."
-    sudo cp "$SCRIPT_DIR/Optimizations/Zram/zram" /bin/zram
-    sudo chmod +x /bin/zram
-    sudo cp "$SCRIPT_DIR/Optimizations/Zram/zram.service" /lib/systemd/system/zram.service
-    sudo systemctl enable --now zram.service
-    echo "ZRAM installed and enabled"
-    ;;
-           4)
-               break
-                  ;;
-            *)
-              # Invalid subchoice
-              echo "Invalid choice"
-              ;;
-          esac
-  done
-else
-  # The user entered an incorrect password
-  echo "Incorrect password"
-  exit
-fi
+        clear
+            print_root_options
+            read -rp "Enter your choice: " subchoice
 
-;;
-3)
-      # Exit the script
-      exit
-      ;;
-*) 
-      # Code to execute if the user enters an invalid choice
-      echo "Invalid choice, please enter 1 or 2 or 3"
-      continue
-      ;;
-  esac
-  
+            case "$subchoice" in
+                1)
+                    # Ask the user for regular or low RAM scenario
+                    echo " "
+                    echo "1. Regular scenario"
+                    echo "2. Low RAM scenario (Only for =<4GB Ram & combine with Zram)"
+                    echo "3. Go back"
+                    read -rp "Enter your choice: " iocache_choice
+
+                    case "$iocache_choice" in
+                        1)
+                            # Copy files for regular scenario and execute sysctl -p
+                            echo "Copying files for regular scenario..."
+                            dir="/etc/udev/rules.d"
+                            dir2="/etc/sysctl.d/"
+                            mkdir -p "$dir" "$dir2"
+                            sudo cp "$SCRIPT_DIR/Optimizations/IO-Schedulers-Cache-Configs/60-ioschedulers.rules" "$dir/60-ioschedulers.rules"
+                            sudo cp "$SCRIPT_DIR/Optimizations/IO-Schedulers-Cache-Configs/sysctl.conf" "$dir2/99-sysctl.conf"
+                            sudo sysctl --system
+                            ;;
+                        2)
+                            # Copy files for low RAM scenario and execute sysctl -p
+                            echo "Copying files for low RAM scenario..."
+                            dir="/etc/udev/rules.d"
+                            dir2="/etc/sysctl.d/"
+                            mkdir -p "$dir" "$dir2"
+                            sudo cp "$SCRIPT_DIR/Optimizations/IO-Schedulers-Cache-Configs/60-ioschedulers.rules" "$dir/60-ioschedulers.rules"
+                            sudo cp "$SCRIPT_DIR/Optimizations/IO-Schedulers-Cache-Configs/sysctl_low_ram.conf" "$dir2/99-sysctl.conf"
+                            sudo sysctl --system
+                            ;;
+                        3)
+                            continue # Go back to the main menu
+                            ;;
+                        *)
+                            # Invalid iocache_choice
+                            echo "Invalid choice"
+                            ;;
+                    esac
+                    ;;
+                2)
+                    # Code for MGLRU option
+                    echo "Installing MGLRU..."
+                    sudo cp "$SCRIPT_DIR/Optimizations/MGLRU/mglru" /bin/mglru
+                    sudo chmod +x /bin/mglru
+                    sudo cp "$SCRIPT_DIR/Optimizations/MGLRU/mglru.service" /lib/systemd/system/mglru.service
+                    sudo systemctl enable --now mglru.service
+                    echo "MGLRU installed and enabled"
+                    ;;
+                3)
+                    # Code for ZRAM option
+                    echo "Installing ZRAM..."
+                    sudo cp "$SCRIPT_DIR/Optimizations/Zram/zram" /bin/zram
+                    sudo chmod +x /bin/zram
+                    sudo cp "$SCRIPT_DIR/Optimizations/Zram/zram.service" /lib/systemd/system/zram.service
+                    sudo systemctl enable --now zram.service
+                    echo "ZRAM installed and enabled"
+                    ;;
+                4)
+                    break
+                    ;;
+                *)
+                    # Invalid subchoice
+                    echo "Invalid choice"
+                    ;;
+            esac
+        done
+    else
+        # The user entered an incorrect password
+        echo "Incorrect password"
+    fi
+}
+
+# Main loop
+while true; do
+clear
+    print_menu
+    read -rp "Enter your choice: " choice
+
+    case "$choice" in
+        1)
+            non_root_options
+            ;;
+        2)
+            root_options
+            ;;
+        3)
+            # Exit the script
+            exit
+            ;;
+        *)
+            # Code to execute if the user enters an invalid choice
+            echo "Invalid choice, please enter 1, 2, or 3"
+            ;;
+    esac
 done
