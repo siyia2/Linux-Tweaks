b931ebaf74f7e71a6d947e2043288a0bb26d28cb
siyia2 <46220960+siyia2@users.noreply.github.com>
2024-07-30 15:22:38 +0300

    Add files via upload

--- Wayland Setup/Sway/waybar/modules/toggle_mute.sh
+++ Wayland Setup/Sway/waybar/modules/toggle_mute.sh
@@ -7,8 +7,8 @@ sink_mute_state=$(pactl get-sink-mute @DEFAULT_SINK@ | awk '{print $2}')
 pactl set-sink-mute @DEFAULT_SINK@ toggle
 
 # Set the mute state of the source to match the new state of the sink
-if [ "$sink_mute_state" = "yes" ]; then
-    pactl set-source-mute alsa_input.pci-0000_00_14.2.analog-stereo 0
-else
+#if [ "$sink_mute_state" = "yes" ]; then
+#    pactl set-source-mute alsa_input.pci-0000_00_14.2.analog-stereo 0
+#else
     pactl set-source-mute alsa_input.pci-0000_00_14.2.analog-stereo 1
-fi
+#fi
