6e77cb9558e76f471ce33030c8dd7389ca7863f3
siyia2 <46220960+siyia2@users.noreply.github.com>
2024-08-06 18:15:19 +0300

    Add files via upload

--- Wayland Setup/Sway/waybar/modules/kb_layout.sh
+++ Wayland Setup/Sway/waybar/modules/kb_layout.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 get_layout() {
-    swaymsg -t get_inputs | awk '/xkb_active_layout_name/{print $2}' | tr -d '"'
+    swaymsg -t get_inputs | awk '/xkb_active_layout_name/{print $2}' | sed 's/^"//;s/"$//'
 }
 
 layout=""
@@ -9,7 +9,7 @@ while true; do
     new_layout=$(get_layout)
     if [ "$new_layout" != "$layout" ]; then
         layout=$new_layout
-        echo $layout
+        echo "$layout"
     fi
     sleep 1
 done
