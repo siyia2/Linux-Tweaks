37cad956c585ea213872818097fe9bafbc4575c0
siyia2 <46220960+siyia2@users.noreply.github.com>
2025-10-01 12:50:41 +0300

    Add files via upload

--- Wayland Setup/Sway/waybar/modules/gpu_power.sh
+++ Wayland Setup/Sway/waybar/modules/gpu_power.sh
@@ -4,7 +4,7 @@ gpu_power=$(cat /sys/class/drm/card1/device/power_dpm_force_performance_level)
 echo "$gpu_power"
 
 if [ "$gpu_power" = "low" ]; then
-    echo "🌿"
+    echo "🌱"
 else
     echo "🚀"
 fi
