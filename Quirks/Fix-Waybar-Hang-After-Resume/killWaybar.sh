a5d9ce560288787b30bf127fa26eede612fdb2de
siyia2 <46220960+siyia2@users.noreply.github.com>
2026-03-11 07:43:30 +0200

    Add files via upload

--- Quirks/Fix-Waybar-Hang-After-Resume/killWaybar.sh
+++ Quirks/Fix-Waybar-Hang-After-Resume/killWaybar.sh
@@ -1,5 +1,8 @@
-#! /bin/sh
+#!/bin/sh
+case "$1" in
+  post)
+    # After resume, signal Waybar as your user
+    su siyia -c "pkill -SIGUSR2 waybar"
+    ;;
+esac
 
- pkill -SIGUSR2 waybar;
-
-exit
