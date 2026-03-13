dfc95236865ab73a39b6a8d9a101444d4552d77d
siyia2 <46220960+siyia2@users.noreply.github.com>
2026-03-13 07:20:23 +0200

    Add files via upload

--- Quirks/Fix-Waybar-Hang-After-Resume/killWaybar.sh
+++ Quirks/Fix-Waybar-Hang-After-Resume/killWaybar.sh
@@ -1,10 +1,15 @@
 #!/bin/sh
-
-# Place the file under /usr/lib/systemd/system-sleep/ and mark it as executable
 case "$1" in
   post)
-    # After resume, signal Waybar as your user
-    su $USER -c "pkill -SIGUSR2 waybar"
+    sleep 1
+    pkill waybar
+    sleep 2
+    SWAYSOCK=$(ls /run/user/1000/sway-ipc.1000.*.sock 2>/dev/null | head -1)
+    sudo -u siyia env \
+      WAYLAND_DISPLAY=wayland-1 \
+      DBUS_SESSION_BUS_ADDRESS=unix:path=/run/user/1000/bus \
+      XDG_RUNTIME_DIR=/run/user/1000 \
+      SWAYSOCK=$SWAYSOCK \
+      waybar &
     ;;
 esac
-
